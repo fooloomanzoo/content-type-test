e4d8bc79c1e12923483d2f57853bffc8a92edeb7
Johannes Brautzsch <hannes-b@web.de>
2020-05-25 12:09:28 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -4,32 +4,30 @@ import * as sapper from '@sapper/server';
 
 const { PORT } = process.env;
 
-const app = express();
+function setHeaders(res, path) {
+  if (path.match(/(png|jpe?g|webp|gif|pdf)$/)) {
+    res.setHeader('Content-Type', blobContentType);
+    res.setHeader('Cache-Control', 'no-store');
+  }
+}
 
 let blobContentType = 'application/octet-stream';
 
-function setHeaders(res, path) {
-	if (path.match(/(png|jpe?g|webp|gif|pdf)$/)) {
-		res.setHeader('Content-Type', blobContentType);
-		res.setHeader('Cache-Control', 'no-store');
-	}
-}
+const app = express();
 
 app.use(express.json());
 
 app.use(express.static('static', { setHeaders }));
 
 app.post('/contentType', (req, res, next) => {
-	blobContentType = req.body.type;
-	res.sendStatus(200);
+  blobContentType = req.body.type;
+  res.sendStatus(200);
 });
 
 app.use(sapper.middleware());
 
-const server = http.createServer(app)
-	
-server.listen(PORT, err => {
-	if (err) {
-		console.error('error', err);
-	}
-})
+http.createServer(app).listen(PORT, err => {
+  if (err) {
+    console.error('error', err);
+  }
+});
